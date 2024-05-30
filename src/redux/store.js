3e873beceebf56427216d8dbab94743881330f67
ricardo-dualboot <ricardo.castillo@dualbootpartners.com>
2024-05-29 22:02:48 -0300

    removing comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,7 @@
 // Store
 import { applyMiddleware, createStore, compose } from 'redux';
 import { rootReducers } from './reducers/rootReducers';
-// import { composeWithDevTools } from 'redux-devtools-extension'
 import { thunk } from 'redux-thunk';
-
-// export const store = createStore(rootReducers, applyMiddleware(thunk));
-/* export const store = createStore(rootReducers, composeWithDevTools(
-    applyMiddleware(thunk)
-)); */
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(rootReducers, composeEnhancers(
